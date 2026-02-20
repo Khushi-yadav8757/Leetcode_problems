545db92d0b74da11d390a19270e0a67c8f04d7e7
Khushi Yadav <kky957242@gmail.com>
2026-02-20 21:56:35 +0530

    Create LargestElement.cpp

---
+++ LargestElement.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5]={4,2,7,1,9};
+    int max = arr[0];
+
+    for(int i=1;i<5;i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+
+    cout<<"Largest = "<<max;
+}
